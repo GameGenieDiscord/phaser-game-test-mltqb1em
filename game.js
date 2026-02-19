662c015563406fdc20b61b128d5c6d296bbc9e6b
GameGenieDiscord <lefhyt64@gmail.com>
2026-02-19 19:25:05 +0200

    Task: make better sprites and assign them correctly to the food

--- game.js
+++ game.js
@@ -12,24 +12,40 @@ let direction = 'right';
 let nextDirection = 'right';
 
 function preload() {
-    // Create simple textures programmatically
+    // Create better textures programmatically
     const graphics = this.make.graphics({ x: 0, y: 0, add: false });
     
-    // Snake head texture
+    // Snake head texture - more detailed
+    graphics.clear();
     graphics.fillStyle(0x00ff00);
-    graphics.fillRect(0, 0, 20, 20);
+    graphics.fillRoundedRect(0, 0, 20, 20, 4);
+    // Add eyes
+    graphics.fillStyle(0xffffff);
+    graphics.fillCircle(6, 6, 2);
+    graphics.fillCircle(14, 6, 2);
+    // Add pupils
+    graphics.fillStyle(0x000000);
+    graphics.fillCircle(6, 6, 1);
+    graphics.fillCircle(14, 6, 1);
     graphics.generateTexture('snake_head', 20, 20);
     
-    // Snake body texture
+    // Snake body texture - gradient effect
     graphics.clear();
-    graphics.fillStyle(0x00cc00);
-    graphics.fillRect(0, 0, 20, 20);
+    graphics.fillGradientStyle(0x00cc00, 0x00cc00, 0x009900, 0x009900, 1);
+    graphics.fillRoundedRect(0, 0, 20, 20, 4);
     graphics.generateTexture('snake_body', 20, 20);
     
-    // Food texture
+    // Food texture - apple-like
     graphics.clear();
-    graphics.fillStyle(0xff0000);
+    // Apple body
+    graphics.fillStyle(0xff3333);
     graphics.fillCircle(10, 10, 8);
+    // Apple leaf
+    graphics.fillStyle(0x33ff33);
+    graphics.fillRect(9, 2, 2, 4);
+    // Apple highlight
+    graphics.fillStyle(0xff9999);
+    graphics.fillCircle(7, 7, 2);
     graphics.generateTexture('food', 20, 20);
 }
 
@@ -42,18 +58,12 @@ function create() {
     
     // Create initial snake (3 segments)
     for (let i = 0; i < 3; i++) {
-        const segment = this.add.rectangle(200 - i * 20, 300, 20, 20, 0x00ff00);
-        if (i === 0) {
-            segment.setFillStyle(0x00ff00); // Head
-        } else {
-            segment.setFillStyle(0x00cc00); // Body
-        }
+        const segment = this.add.image(200 - i * 20, 300, i === 0 ? 'snake_head' : 'snake_body');
         snake.add(segment);
     }
     
-    // Create food
-    food = this.add.rectangle(400, 300, 20, 20, 0xff0000);
-    food.setStrokeStyle(2, 0xffffff);
+    // Create food with better sprite
+    food = this.add.image(400, 300, 'food');
     
     // Score text
     scoreText = this.add.text(16, 16, 'Score: 0', {
@@ -161,6 +171,22 @@ function moveSnake() {
     head.x = newX;
     head.y = newY;
     
+    // Rotate head based on direction
+    switch (direction) {
+        case 'left':
+            head.setRotation(-Math.PI / 2);
+            break;
+        case 'right':
+            head.setRotation(Math.PI / 2);
+            break;
+        case 'up':
+            head.setRotation(0);
+            break;
+        case 'down':
+            head.setRotation(Math.PI);
+            break;
+    }
+    
     // Check food collision
     if (Math.abs(head.x - food.x) < 15 && Math.abs(head.y - food.y) < 15) {
         eatFood.call(this);
@@ -174,7 +200,7 @@ function eatFood() {
     
     // Add new segment to snake
     const tail = snake.getChildren()[snake.getChildren().length - 1];
-    const newSegment = this.add.rectangle(tail.x, tail.y, 20, 20, 0x00cc00);
+    const newSegment = this.add.image(tail.x, tail.y, 'snake_body');
     snake.add(newSegment);
     
     // Move food to new random position
